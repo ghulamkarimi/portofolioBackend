ef1ab844a7547cc274a9bdcd588843687d6e9ad7
ghulamkarimi <karimiamirhossein12@gmail.com>
2025-02-17 13:47:15 +0100

    update CORS configuration to include allowed methods

--- index.ts
+++ index.ts
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(
     cors({
         origin: ['https://ai-webkraft.de', 'https://api.ai-webkraft.de'],
-        credentials: true
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
 
